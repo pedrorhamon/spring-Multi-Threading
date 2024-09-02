921b5ef3101b06aa786661155566420102a4992d
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-09-02 19:18:15 -0300

    criando exemplo de thread

--- src/main/java/com/starking/multitreading/service/TaskService.java
+++ src/main/java/com/starking/multitreading/service/TaskService.java
@@ -1,5 +1,6 @@
 package com.starking.multitreading.service;
 
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 /**
@@ -8,5 +9,20 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TaskService {
+
+	@Async
+	public void executeTask() {
+		System.out.println("Task execution started in thread: " + Thread.currentThread().getName());
+
+		// Simulating a long-running task
+		try {
+			Thread.sleep(5000); // 5 seconds delay
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			throw new IllegalStateException(e);
+		}
+
+		System.out.println("Task execution completed in thread: " + Thread.currentThread().getName());
+	}
 
 }
