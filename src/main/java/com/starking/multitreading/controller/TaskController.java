ab00699eaac1aa265cb9558f2e75590cfd60b5e9
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-09-02 19:16:07 -0300

    casca do endpoint

---
+++ src/main/java/com/starking/multitreading/controller/TaskController.java
@@ -0,0 +1,13 @@
+package com.starking.multitreading.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author pedroRhamon
+ */
+@RequestMapping("/api/tasks")
+@RestController
+public class TaskController {
+
+}
