04228c8bf2755c4cdfea5841f068d2deffeef9a4
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-09-02 19:17:03 -0300

    criando metodo

--- src/main/java/com/starking/multitreading/controller/TaskController.java
+++ src/main/java/com/starking/multitreading/controller/TaskController.java
@@ -1,13 +1,26 @@
 package com.starking.multitreading.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.starking.multitreading.service.TaskService;
+
+import lombok.AllArgsConstructor;
 
 /**
  * @author pedroRhamon
  */
 @RequestMapping("/api/tasks")
 @RestController
+@AllArgsConstructor
 public class TaskController {
 
+	private final TaskService taskService;
+
+	@GetMapping("/process")
+	public String processTask() {
+		taskService.executeTask();
+		return "Task is being processed in the background!";
+	}
 }
